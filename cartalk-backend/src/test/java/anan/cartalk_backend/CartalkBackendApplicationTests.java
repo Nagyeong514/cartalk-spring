47e53920eb4a560e027247ae72d0823f95906ca6
An <dksskrud1027@gmail.com>
2026-01-28 08:11:51 +0900

    feat: Add backend

---
+++ cartalk-backend/src/test/java/anan/cartalk_backend/CartalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package anan.cartalk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
