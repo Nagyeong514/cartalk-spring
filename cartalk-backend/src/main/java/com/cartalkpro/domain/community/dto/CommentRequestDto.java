aa06d029521f6253b39e7d51ebd3809f96ee5206
An <dksskrud1027@gmail.com>
2026-03-13 11:41:13 +0900

    feat: Community

--- cartalk-backend/src/main/java/com/cartalkpro/domain/community/dto/CommentRequestDto.java
+++ cartalk-backend/src/main/java/com/cartalkpro/domain/community/dto/CommentRequestDto.java
@@ -1,10 +1,12 @@
 package com.cartalkpro.domain.community.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CommentRequestDto {
+    @NotBlank(message = "댓글 내용을 입력해주세요.") // 유효성 검사
     private String content;
 }
